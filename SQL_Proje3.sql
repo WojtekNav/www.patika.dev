ef5ddf975b4664c6ed5871172d2ce3eb093fbf39
WojtekNav <109801954+WojtekNav@users.noreply.github.com>
2022-07-27 17:56:02 +0300

    Create SQL_Proje3.sql

---
+++ SQL_Proje3.sql
@@ -0,0 +1,11 @@
+--SELECT * FROM country
+--WHERE country ILIKE 'a%a';
+
+--select * from country
+--where country like '_____%n';
+
+--select * from film
+--where title ilike '%t%t%t%t%';
+
+--select * from film
+--where title like 'C%' AND length > 90 AND rental_rate=2.99;
